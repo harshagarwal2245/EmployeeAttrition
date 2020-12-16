2bb211eea4313df26f4c8bdab7f1e16135465fb5
Harsh Agarwal <agarwalharsh244@gmail.com>
2020-12-15 22:24:07 -0800

    Created Architecture
    Created files and done neccessary imports

---
+++ EmployeeAttrition.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 22:20:18 2020
+
+@author: Harsh_12
+"""
+
+from pydantic import BaseModel
+
+class Employee(BaseModel):
+    
---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 15 22:17:08 2020
+
+@author: Harsh_12
+"""
+
+import uvicorn
+from fastapi import FastAPI 
+from EmployeeAttrition import Employee
+import numpy as np
+import pandas as od
+import joblib
